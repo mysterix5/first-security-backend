5f73a7625c5b22312e565886c097bc05d175c765
Lukas <lukasduechting@mailbox.org>
2022-07-02 11:57:39 +0200

    React router fix

---
+++ src/main/java/de/neuefische/kanbanbackend/config/ReactRouterFix.java
@@ -0,0 +1,11 @@
+package de.neuefische.kanbanbackend.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ReactRouterFix {
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String forwardToRouteUrl() {
+        return "forward:/";
+    }
+}
