00129338e2bf2a9109ff41d3183fb97aab9c79c5
Lukas <lukasduechting@mailbox.org>
2022-07-02 12:09:04 +0200

    React router fix only for oauth

--- src/main/java/de/neuefische/kanbanbackend/config/ReactRouterFix.java
+++ src/main/java/de/neuefische/kanbanbackend/config/ReactRouterFix.java
@@ -4,8 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReactRouterFix {
-    @RequestMapping(value = "/**/{[path:[^\\.]*}")
-    public String forwardToRouteUrl() {
-        return "forward:/";
+//    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+//    public String forwardToRouteUrl() {
+//        return "forward:/";
+//    }
+    @RequestMapping(value = "/oauth")
+    public String forwardToOauth() {
+        return "forward:/oauth";
     }
 }
